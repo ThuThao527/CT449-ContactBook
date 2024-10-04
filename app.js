fb8789d33dad055a0bf858eb6b2e326fb7136869
ThuThao527 <thuthao291204@gmail.com>
2024-10-04 10:48:56 +0700

    Cai dat express chay thong bao xin chao

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.get("/", (req, res) => {
+	res.json({ message: "Welcome to contact book application." });
+});
+
+module.exports = app;
